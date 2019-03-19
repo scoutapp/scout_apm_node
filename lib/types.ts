29489398f785ba0ce5fd7c15c96b3b36fd541332
vados-scout <vados+scout@vadosware.io>
2019-03-19 10:44:00 +0900

    Move types to lib, bugfixes

--- lib/types.ts
+++ lib/types.ts
@@ -1,11 +1,22 @@
-enum AgentType = {
+const DOMAIN_SOCKET_URI_SCHEME = "http+unix://";
+
+enum AgentType {
     Process = "process",
     ChildProcess = "child-process",
 }
 
+class AgentMessage {
+    contentLength: number;
+    contents: Buffer;
+}
+
+class AgentResponse {
+    contents: Buffer;
+}
+
 interface Agent<T> {
-    public getType(): T;
-    public send(msg: AgentMessage): Promise<AgentResponse>;
+    getType(): T;
+    send(msg: AgentMessage): Promise<AgentResponse>;
 }
 
 /**
@@ -13,28 +24,18 @@ interface Agent<T> {
  *
  * @param {T} T type of the agent
  */
-abstract class AgentOptions<T> {
-    agentType: T;
-
-    constructor(agentType: T) {
-        this.agentType = agentType;
-    }
-}
-
-const DOMAIN_SOCKET_URI_SCHEME = "http+unix://";
+type AgentOptions = ProcessOptions | ChildProcessOptions;
 
 /**
  * Contact options for agents in a local process
  */
-class ProcessOptions extends AgentContactOptions<AgentContactMethod.Process> {
+class ProcessOptions {
     /// URI of the process
     public readonly uri: string;
     // Port of the agent process
     public readonly port?: number;
 
     constructor(uri: string, port?: number) {
-        this.super(AgentContactMethod.Process);
-
         this.uri = uri;
         if (port) { this.port = port; }
     }
@@ -44,20 +45,19 @@ class ProcessOptions extends AgentContactOptions<AgentContactMethod.Process> {
      *
      * @returns {boolean} whether the address is a domain socket
      */
-    public isDomainSocket() {
-        return this.address.startsWith(DOMAIN_SOCKET_URI_SCHEME);
+    public isDomainSocket(): boolean {
+        return this.uri.startsWith(DOMAIN_SOCKET_URI_SCHEME);
     };
 }
 
 /**
  * Options for agents that must be spawned in a child process
  */
-class ProcessOptions extends AgentContactOptions<AgentContactMethod.ChildProcess> {
+class ChildProcessOptions {
     // Path to the binary
     public readonly binPath: string;
 
     constructor(binPath: string) {
-        this.super(AgentContactMethod.ChildProcess);
         this.binPath = binPath;
     }
 }
